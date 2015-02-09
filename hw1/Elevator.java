54cb67501e742994577cef81049b107bbad400e8
Yves Yuen <yves.j.yuen@gmail.com>
2015-02-08 23:30:40 -0800

    added move(), boardPassenger(), unload() and toString() methods

--- hw1/Elevator.java
+++ hw1/Elevator.java
@@ -10,6 +10,7 @@ public class Elevator
 	private int direction = UP;
 	private int currentFloor = 1;
 	private int [] passengersToFloor = new int [FLOOR_NUMBERS];
+	private int totalPassengerNumber= 0;
 
 
 	//Constructor
@@ -31,19 +32,78 @@ public class Elevator
 		direction = DOWN;
 	}
 
+
 	public void boardPassenger(int floor)
 	{
+		passengersToFloor[floor] += 1; 
 
 	}
 
-	public String toString()
+	public void move()
 	{
-		return currentFloor + " :" + "" + " passengers";
+	//	for(int i = 0; i < passengersToFloor.length; i+=direction)
+		for(int i = 0; i < FLOOR_NUMBERS; i++)	
+		{
+			this.unload(i);
+			this.toString(i);
+		}
+	}
+
+	public void unload(int floor)
+	{
+		passengersToFloor[floor] = 0;
+	}
+
+	public void toString(int floor)
+	{
+		String plural; 
+		if (getTotalPassengerNumber() == 1)
+		{
+			plural = "s";
+		} 
+		else
+		{
+			plural ="";
+		}
+		System.out.println("Floor " + floor + " : " +  this.getTotalPassengerNumber() + " passenger" + plural + ".");
+	}
+
+	// Getter method
+	public int getDirection()
+	{
+		return direction;
+	}
+
+	public int getPassengerNumbertoFloor(int floor)
+	{
+		return passengersToFloor[floor];
+	}
+
+	public int getTotalPassengerNumber()
+	{
+		totalPassengerNumber = 0;
+		for(int i = 0; i < passengersToFloor.length; i++)
+			{
+					totalPassengerNumber += passengersToFloor[i];
+			}
+
+		return totalPassengerNumber;
 	}
 
 	public static void main(String argv[])
 	{
 		Elevator elevator = new Elevator();
-		System.out.println("hi");
+
+		elevator.boardPassenger(3);
+		elevator.boardPassenger(3);
+		elevator.boardPassenger(5);
+
+		for(int i = 0; i < FLOOR_NUMBERS; i++)
+		{
+			System.out.println(elevator.getPassengerNumbertoFloor(i));
+		}
+
+		System.out.println("number of passenger: "  + elevator.getTotalPassengerNumber());
+		elevator.move();
 	}
 }
